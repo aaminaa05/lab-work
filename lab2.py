e7da29fe1fd720e7437ebbe2045e4d9c0ef3c385
aaminaa05 <157339228+aaminaa05@users.noreply.github.com>
2024-01-27 00:28:22 +0600

    Add files via upload

---
+++ lab2.py
@@ -0,0 +1,406 @@
+#BOOLEAN
+#examples
+print(bool("Hello"))
+print(bool(15))
+
+bool("abc")
+bool(123)
+bool(["apple", "cherry", "banana"])
+
+bool(False)
+bool(None)
+bool(0)
+bool("")
+bool(())
+bool([])
+bool({})
+
+def myFunction() :
+  return True
+
+print(myFunction())
+
+x = 200
+print(isinstance(x, int))
+
+#exercise
+
+print(10 > 9)
+True
+
+print(10==9)
+False
+
+print(10 < 9)
+False
+
+print(bool("abc"))
+True
+
+print(bool(0))
+False
+
+#OPERATORS
+#examples
+print(10 + 5)
+
+print((6 + 3) - (6 + 3))
+
+print(100 + 5 * 3)
+
+print(5 + 4 - 7 + 3)
+
+#exercise
+
+print(10 * 5)
+
+print(10 / 2)
+
+fruits = ["apple", "banana"]
+if "apple" in fruits:
+  print("Yes, apple is a fruit!")
+
+if 5 != 10:
+  print("5 and 10 is not equal")
+
+if 5 == 10 or 4 == 4:
+  print("At least one of the statements is true")
+
+#LISTS
+#examples
+
+thislist = ["apple", "banana", "cherry"]
+tropical = ["mango", "pineapple", "papaya"]
+thislist.extend(tropical)
+print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+thislist.insert(2, "watermelon")
+print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+print(len(thislist))
+
+mylist = ["apple", "banana", "cherry"]
+print(type(mylist))
+
+thislist = ["orange", "mango", "kiwi", "pineapple", "banana"]
+thislist.sort()
+print(thislist)
+
+#exercise
+  
+fruits = ["apple", "banana", "cherry"]
+print(fruits[1])
+
+fruits = ["apple", "banana", "cherry"]
+fruits[0] = "kiwi"
+
+fruits = ["apple", "banana", "cherry"]
+fruits.append("orange")
+
+fruits = ["apple", "banana", "cherry"]
+fruits.insert(1,"lemon")
+
+fruits = ["apple", "banana", "cherry"]
+fruits.remove("banana")
+
+fruits = ["apple", "banana", "cherry"]
+print(fruits[-1])
+
+fruits = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(fruits[2:5])
+
+fruits = ["apple", "banana", "cherry"]
+print(len(fruits))
+
+#TUPLES
+#examples
+thistuple = ("apple", "banana", "cherry")
+print(thistuple)
+
+thistuple = ("apple", "banana", "cherry")
+print(thistuple[1])
+
+thistuple = ("apple", "banana", "cherry")
+y = list(thistuple)
+y.append("orange")
+thistuple = tuple(y)
+
+thistuple = ("apple", "banana", "cherry")
+del thistuple
+print(thistuple)
+
+#exercise
+
+fruits = ("apple", "banana", "cherry")
+print(fruits[0])
+
+fruits = ("apple", "banana", "cherry")
+print(len(fruits))
+
+fruits = ("apple", "banana", "cherry")
+print(fruits[-1])
+
+fruits = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(fruits[2:5])
+
+#SETS
+#examples
+thisset = {"apple", "banana", "cherry"}
+print(thisset)
+
+thisset = {"apple", "banana", "cherry"}
+print(len(thisset))
+
+thisset = {"apple", "banana", "cherry"}
+thisset.add("orange")
+print(thisset)
+
+thisset = {"apple", "banana", "cherry"}
+tropical = {"pineapple", "mango", "papaya"}
+thisset.update(tropical)
+print(thisset)
+
+thisset = {"apple", "banana", "cherry"}
+mylist = ["kiwi", "orange"]
+thisset.update(mylist)
+print(thisset)
+#exercise
+
+fruits = {"apple", "banana", "cherry"}
+if "apple" in fruits:
+  print("Yes, apple is a fruit!")
+
+fruits = {"apple", "banana", "cherry"}
+fruits.add("orange")
+
+fruits = {"apple", "banana", "cherry"}
+more_fruits = ["orange", "mango", "grapes"]
+fruits.update(more_fruits)
+
+fruits = {"apple", "banana", "cherry"}
+fruits.remove("banana")
+
+fruits = {"apple", "banana", "cherry"}
+fruits.discard("banana")
+
+#DICTIONARIES
+#examples
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+if "model" in thisdict:
+  print("Yes, 'model' is one of the keys in the thisdict dictionary")
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict["year"] = 2018
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict["color"] = "red"
+print(thisdict)
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict.pop("model")
+print(thisdict)
+
+#exercise
+
+car =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(car.get("model"))
+
+car =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+car[year] = 2020
+
+car =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+car["color"] = "red"
+
+car =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+car.pop("model")
+
+car =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+car.clear()
+
+#IFELSE
+#examples
+a = 33
+b = 200
+if b > a:
+  print("b is greater than a")
+
+a = 33
+b = 33
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+
+a = 200
+b = 33
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
+
+if a > b: print("a is greater than b")
+
+a = 330
+b = 330
+print("A") if a > b else print("=") if a == b else print("B")
+#exercise
+
+a = 50
+b = 10
+if a > b:
+  print("Hello World")
+
+a = 50
+b = 10
+if a != b:
+  print("Hello World")
+
+a = 50
+b = 10
+if a == b:
+  print("Yes")
+else:
+  print("No")
+
+a = 50
+b = 10
+if a == b:
+  print("1")
+elif a > b:
+  print("2")
+else:
+  print("3")
+
+if a == b and c == d:
+  print("Hello")
+
+if a == b or c == d:
+  print("Hello")
+
+if 5 > 2:
+  print("YES")
+
+a = 2
+b = 5
+print("YES") if a == b else print("NO")
+
+a = 2
+b = 50
+c = 2
+if a == c or b == c:
+  print("YES")
+
+#WHILE
+#examples
+i = 1
+while i < 6:
+  print(i)
+  i += 1
+else:
+  print("i is no longer less than 6")
+
+#exercise
+i = 1
+while i < 6:
+  print(i)
+  i += 1
+
+i = 1
+while i < 6:
+  if i == 3:
+    break
+  i += 1
+
+i = 0
+while i < 6:
+  i += 1
+  if i == 3:
+    continue
+  print(i)
+
+i = 1
+while i < 6:
+  print(i)
+  i += 1
+else:
+  print("i is no longer less than 6")
+
+#FOR LOOPS
+#examples
+for x in range(6):
+  print(x)
+else:
+  print("Finally finished!")
+
+adj = ["red", "big", "tasty"]
+fruits = ["apple", "banana", "cherry"]
+
+for x in adj:
+  for y in fruits:
+    print(x, y)
+
+
+#exercise
+  
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+    continue
+  print(x)
+
+for x in range:
+  print(x)
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+    break
+  print(x)
